d53af601675d18825054364ba879f0836a049aed
Edward Brekelbaum <nedbrek@yahoo.com>
2016-05-05 07:49:52 -0500

    scripts/test.tcl: Added support for other directories

--- scripts/test.tcl
+++ scripts/test.tcl
@@ -1,13 +1,27 @@
 #!/usr/bin/env tclsh
-cd game1b
-set turns [lsort -integer [regsub -all {turn} [glob "turn*"] {}]]
+proc testGame {dir} {
+	cd $dir
+	set turns [lsort -integer [regsub -all {turn} [glob "turn*"] {}]]
 
-foreach t $turns {
-	puts "turn$t"
-	cd turn$t
-	exec diff . ../../game1/turn$t
-	exec ../../ceran.exe run > /dev/null
-	exec diff . ../../game1/turn$t
-	cd ..
+	set otherDir [string range $dir 0 end-1]
+
+	foreach t $turns {
+		puts "turn$t"
+		cd turn$t
+		exec diff . ../../$otherDir/turn$t
+		exec ../../ceran.exe run > /dev/null
+		exec diff . ../../$otherDir/turn$t
+		cd ..
+	}
+}
+
+if {$argc == 0} {
+	set dirs [list "game1b"]
+} else {
+	set dirs $argv
+}
+
+foreach d $dirs {
+	testGame $d
 }
 
