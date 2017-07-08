901bf5812be4830e08a81b30075f5cbe7da0c739
Edward Brekelbaum <nedbrek@yahoo.com>
2017-07-07 20:49:36 -0500

    Moved item enable/disable to a yaml

--- py_atlantis/atlantis.py
+++ py_atlantis/atlantis.py
@@ -8,97 +8,21 @@ def setRules(game, game_name):
         print "Only ceran supported right now"
         sys.exit()
 
-    game.enableItem("PICK")
-    game.enableItem("SPEAR");
-    game.enableItem("AXE");
-    game.enableItem("HAMMER");
-    game.enableItem("MXBO");
-    game.enableItem("MWAG");
-    game.enableItem("GLIDER");
-    game.enableItem("NET");
-    game.enableItem("LASSO");
-    game.enableItem("BAG");
-    game.enableItem("SPIN");
-    game.enableItem("LEATHER ARMOR");
-    game.enableItem("CLOTH ARMOR");
-    game.enableItem("BOOT");
+    import yaml
+    import os
+    f = open(os.path.dirname(__file__) + "/" + game_name + ".yaml")
+    data_map = yaml.safe_load(f)
+    f.close()
 
-    game.enableItem("PIRATES");
-    game.enableItem("KRAKEN");
-    game.enableItem("MERFOLK");
-    game.enableItem("ELEMENTAL");
+    cfg = data_map['config']
+    if "items" in cfg:
+        if "enable" in cfg["items"]:
+            for i in cfg["items"]["enable"]:
+                game.enableItem(i)
 
-    game.enableItem("FAIRY");
-    game.enableItem("LIZA");
-    game.enableItem("URUK");
-    game.enableItem("GBLN");
-    game.enableItem("HALFLING");
-    game.enableItem("GNOLL");
-    game.enableItem("DRLF");
-    game.enableItem("MERC");
-    game.enableItem("TITAN");
-    game.enableItem("AMAZON");
-    game.enableItem("OGER");
-    game.enableItem("GNOME");
-    game.enableItem("MOUNTAINMAN");
-    game.enableItem("MINOTAUR");
-    game.enableItem("GREY ELF");
-
-    game.enableItem("LANCE");
-    game.enableItem("MUSHROOM");
-
-    game.enableItem("RRAT");
-    game.enableItem("NOOGLE");
-    game.enableItem("MUTANT");
-
-    game.enableItem("BAXE");
-    game.enableItem("MBAX");
-    game.enableItem("ADMT");
-    game.enableItem("ASWR");
-    game.enableItem("ABAX");
-    game.enableItem("IMTH");
-    game.enableItem("ARNG");
-    game.enableItem("AARM");
-    game.enableItem("CAMEL");
-
-    game.enableItem("DROW");
-    game.enableItem("HYDR");
-    game.enableItem("STORM GIANT");
-    game.enableItem("CLOUD GIANT");
-    game.enableItem("ILLYRTHID");
-    game.enableItem("EVIL SORCERER");
-    game.enableItem("EVIL MAGICIAN");
-    game.enableItem("DARK MAGE");
-    game.enableItem("EVIL WARRIORS");
-    game.enableItem("ICE DRAGON");
-
-    game.enableItem("HEALING POTION");
-    game.enableItem("ROUGH GEM");
-    game.enableItem("GEMS");
-    game.enableItem("JAVELIN");
-    game.enableItem("PIKE");
-    game.enableItem("MWOL");
-    game.enableItem("MSPI");
-    game.enableItem("MOLE");
-    game.enableItem("BPLA");
-    game.enableItem("FSWO");
-    game.enableItem("MCAR");
-    game.enableItem("QUAR");
-    game.enableItem("SABRE");
-    game.enableItem("MACE");
-    game.enableItem("MSTA");
-    game.enableItem("DAGGER");
-    game.enableItem("PDAG");
-    game.enableItem("BHAM");
-    game.enableItem("SHORTBOW");
-    game.enableItem("BOW");
-    game.enableItem("HEAVY CROSSBOW");
-    game.enableItem("HARP");
-
-    game.enableItem("TAROT CARDS", False);
-    game.enableItem("SUPER BOW", False);
-    game.enableItem("DOUBLE BOW", False);
-    # end
+        if "disable" in cfg["items"]:
+            for i in cfg["items"]["disable"]:
+                game.enableItem(i, False)
 
 if __name__ == '__main__':
     # check arguments
