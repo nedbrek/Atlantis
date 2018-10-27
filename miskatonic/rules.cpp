f550c4cce377259a7b9e5efa91bc8c85add3f88f
Mochnant <mochnant@gmail.com>
2018-10-26 21:21:19 -0400

    Miskatonic rulesset: Increased mages/apprentices from maximum 2 to 3.

--- miskatonic/rules.cpp
+++ miskatonic/rules.cpp
@@ -38,11 +38,11 @@
 // the html file containing the rules to correctly reflect the changes!
 //
 
-static int am[] = { 2, 2, 2, 2, 2, 2, 2 };
+static int am[] = { 3, 3, 3, 3, 3, 3, 3 };
 int *allowedMages = am;
 int allowedMagesSize = sizeof(am) / sizeof(am[0]);
 
-static int aa[] = { 2, 2, 2, 2, 2, 2, 2 };
+static int aa[] = { 3, 3, 3, 3, 3, 3, 3 };
 int *allowedApprentices = aa;
 int allowedApprenticesSize = sizeof(aa) / sizeof(aa[0]);
 
@@ -56,7 +56,7 @@ int allowedTradesSize = sizeof(at) / sizeof(at[0]);
 
 static GameDefs g = {
 	"Miskatonic",				// RULESET_NAME
-	MAKE_ATL_VER( 1, 0, 0 ),    // RULESET_VERSION
+	MAKE_ATL_VER( 1, 0, 1 ),    // RULESET_VERSION
 
 	2, /* FOOT_SPEED */
 	4, /* HORSE_SPEED */
