4a9a42ef85ed831c024570062cfb8156215523bc
Edward Brekelbaum <nedbrek@yahoo.com>
2019-01-19 10:06:10 -0600

    Fix #71 sharing description problem    Added to Unit::TemplateReport()

--- unit.cpp
+++ unit.cpp
@@ -638,6 +638,7 @@ AString Unit::TemplateReport()
 	if (GetFlag(FLAG_CONSUMING_UNIT)) temp += ", consuming unit's food";
 	if (GetFlag(FLAG_CONSUMING_FACTION)) temp += ", consuming faction's food";
 	if (GetFlag(FLAG_NOCROSS_WATER)) temp += ", won't cross water";
+	if (GetFlag(FLAG_SHARE)) temp += ", sharing";
 	temp += SpoilsReport();
 
 	temp += items.Report(2, 1, 0);
